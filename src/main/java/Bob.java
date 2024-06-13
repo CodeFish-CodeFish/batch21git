d26e24ba223b3cb4f8b35d44cfef513b137de550
CodeFish <codefish.1class@gmail.com>
2024-06-12 20:45:57 -0500

    stashing message

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -2,5 +2,6 @@ public class Bob {
     public static void main(String[] args) {
         System.out.println("1 message from git ");
         System.out.println("Git add after add");
+        System.out.println("Message from stash");
     }
 }
